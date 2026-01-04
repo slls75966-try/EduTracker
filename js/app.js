51ab5f2d624e03e3e19484146867675624153f0e
slls75966-try <slls75966@gmail.com>
2026-01-04 23:07:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-alert("Le script est bien chargé !");
+// On attend que la page soit prête
+window.onload = function() {
+    const dateElement = document.getElementById('current-date');
+    
+    if (dateElement) {
+        const options = { 
+            weekday: 'long', 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric' 
+        };
+        const today = new Date().toLocaleDateString('fr-FR', options);
+        
+        // Mise à jour du texte
+        dateElement.textContent = today;
+        console.log("Date chargée !");
+    } else {
+        console.error("Erreur : L'ID 'current-date' est introuvable dans le HTML.");
+    }
+};
